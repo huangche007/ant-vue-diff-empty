6ed3b22acc5ad05d0138f90237a8c58360f943bd
huangche <huangche@cohl.com>
2021-12-26 14:35:36 +0800

    init project

--- src/App.vue
+++ src/App.vue
@@ -7,93 +7,15 @@
  * @FilePath: \videoMonitoringSystem\src\App.vue
 -->
 <template>
-  <a-config-provider :locale="mLocale">
+  <a-config-provider>
     <router-view />
   </a-config-provider>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref, watch } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-import { store } from '@ice/stark-data'
-import { SETLOCALE } from '@/assets/language'
-import { getLanguage } from '@/utils/checkBroswer'
-import { getStore, setStore } from '@/utils/storage'
-import { useStore } from 'vuex'
-import enUS from 'ant-design-vue/es/locale/en_US'
-import zhCN from 'ant-design-vue/es/locale/zh_CN'
-import moment from 'moment'
-import 'moment/dist/locale/zh-cn'
-import { mapGetters } from 'vuex'
-moment.locale('zh')
+import { defineComponent } from 'vue'
 export default defineComponent({
-  computed: {
-    ...mapGetters('user', ['isFirstLogin']),
-  },
-  setup() {
-    let mLocale: any = null
-    const route = useRoute()
-    const router = useRouter()
-    const initPwdShow = ref<boolean>(false)
-    const isFirstLogin = Number(getStore('isFirstLogin'))
-    const changeLanguage = () => {
-      store.on(
-        'language',
-        (language) => {
-          if (language) {
-            SETLOCALE(language)
-          }
-        },
-        true
-      )
-      const locale = window.localStorage.getItem('locale')
-      if (locale) {
-        if (locale === 'zh-CN') {
-          mLocale = ref(zhCN)
-        } else {
-          mLocale = ref(enUS)
-        }
-      } else {
-        if (getLanguage() === 'zh-CN') {
-          mLocale = ref(zhCN)
-        } else {
-          mLocale = ref(enUS)
-        }
-      }
-    }
-    changeLanguage()
-    const changeColor = () => {
-      const defaultMainColor: string = store.get('defaultMainColor')
-      if (defaultMainColor) {
-        window['less'].modifyVars({
-          '@primary-color': defaultMainColor,
-          '@error-color': defaultMainColor,
-        })
-      }
-    }
-    watch(
-      () => route.path,
-      (curPath: string | undefined) => {
-        if (curPath) {
-          if (curPath.includes('undefined')) {
-            curPath = curPath.replace('/undefined', '')
-            router.replace(curPath)
-          }
-          changeColor()
-        }
-      }
-    )
-    onMounted(() => {
-      changeColor()
-      if (isFirstLogin === 1) {
-        initPwdShow.value = true
-      }
-    })
-    return {
-      mLocale,
-      initPwdShow,
-    }
-  },
+  setup() {},
 })
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import { createApp, watch } from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
-import {
-  createRouter,
-  createWebHashHistory,
-  createWebHistory
-} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import routes from "./router";
 import "./public-path";
 import { install } from "./locale";
@@ -14,13 +10,6 @@ import { useComponent } from "@/utils/antdvRegistry.js";
 import i18n from "@/assets/language";
 import "@/styles/index.less";
 let app, router;
-
-declare global {
-  interface Window {
-    __POWERED_BY_QIANKUN__: boolean;
-  }
-}
-const __qiankun__ = window.__POWERED_BY_QIANKUN__;
 
 export async function bootstrap(props) {}
 
@@ -30,7 +19,7 @@ export async function mount(props) {
   useComponent(app);
   router = createRouter({
     // history: createWebHashHistory(__qiankun__ ? '/auth/' : '/'),
-    history: createWebHistory(__qiankun__ ? "/auth/" : "/"),
+    history: createWebHistory("/"),
     routes
   });
   app
@@ -43,5 +32,4 @@ export async function unmount(props) {
   app = null;
   router = null;
 }
-
-__qiankun__ || mount({});
+mount({});
