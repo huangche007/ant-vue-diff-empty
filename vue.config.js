539cfa766b8c7ac19db00d48e132d1fba21c76e5
huangche <huangche@cohl.com>
2021-12-28 14:48:55 +0800

    样式丢失问题处理

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ const HashedModuleIdsPlugin = require('webpack').HashedModuleIdsPlugin
 const CompressionWebpackPlugin = require('compression-webpack-plugin')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
-require("./color");
+// require("./color");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 function resolve(dir) {
   return path.join(__dirname, dir)
