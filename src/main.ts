1663f59919ab4228ae2bca73ecfcfd96eab104a5
huangche <huangche@cohl.com>
2021-12-28 14:46:53 +0800

    样式丢失问题处理

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { createRouter, createWebHistory } from "vue-router";
 import routes from "./router";
-import "./styles/index.less";
 import { useComponent } from "@/utils/antdvRegistry.js";
 let app, router;
 // eslint-disable-next-line prefer-const
